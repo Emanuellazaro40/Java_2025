9f650064911ed0c16c311f70d7a7504b6e41bcb9
Emanuellazaro40 <164494230+Emanuellazaro40@users.noreply.github.com>
2025-09-18 15:45:56 +0100

    Add files via upload

---
+++ FirstProject/src/FirstProject.java
@@ -0,0 +1,19 @@
+public class FirstProject {
+    public static void main(String[] args){
+        String nome = "Daniel";
+        String morada = "Rua do Matadouro 14 3080-014 Figueira da Foz";
+        int idade = 25;
+        char[] estadoCivil = {'C', 'S', 'V', 'D'};
+
+        double media = (15.75 + 12.25)/2;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Morada: " + morada);
+        System.out.println("Idade: " + idade);
+        System.out.println("Estado Civil: " + estadoCivil[1]);
+        System.out.println("Media: " + media);
+
+        System.out.println("Nome: " + nome +", Morada: " + morada +", Idade: " + idade +",Estado Civil: " + estadoCivil[1] +",Media: " + media );
+
+    }
+}
