aee821a9d9b428b9cda6835072037acffafa477a
Emanuellazaro40 <164494230+Emanuellazaro40@users.noreply.github.com>
2025-09-23 15:05:13 +0100

    Add files via upload

---
+++ Atividade_3/src/Main.java
@@ -0,0 +1,11 @@
+public  class Main{
+    public static void main(String[] args){
+        int valor = 6;
+
+        if(valor <= 5){
+            System.out.println("O livro é barrato! 😁");
+        }else{
+            System.out.println("O livro é carro! 😒😒");
+        }
+    }
+}
