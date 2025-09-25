99cb6ad89fa975f182c42f710d10e3e4f47e2716
Emanuellazaro40 <164494230+Emanuellazaro40@users.noreply.github.com>
2025-09-25 15:20:41 +0100

    Add files via upload

---
+++ Atividade_4/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args){
+        char caracter ='A';
+
+       if(caracter == 'a' ||
+               caracter =='A'||
+               caracter== 'e' ||
+               caracter== 'E' ||
+               caracter == 'i' ||
+               caracter== 'I' ||
+               caracter=='o' ||
+               caracter== 'O' ||
+               caracter== 'u' ||
+               caracter=='U'
+       ){
+
+           System.out.println("É vogal");
+
+       }else {
+           System.out.println("É uma consoante");
+       }
+
+    }
+}
