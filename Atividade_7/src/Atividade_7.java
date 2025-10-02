736b0e488e8e31849addfb8d0255cc23a08a5607
Emanuellazaro40 <164494230+Emanuellazaro40@users.noreply.github.com>
2025-10-02 16:09:47 +0100

    Add files via upload

---
+++ Atividade_7/src/Atividade_7.java
@@ -0,0 +1,10 @@
+public class Atividade_7 {
+    public static void main(String[] args){
+        int v = 4;
+
+        String n = 5 > v? "True" : "False";
+
+        System.out.println(n);
+
+    }
+}
