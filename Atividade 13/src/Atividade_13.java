c0c18af2af000a92c6ef996e33b12d00c5cadcdb
Emanuellazaro40 <164494230+Emanuellazaro40@users.noreply.github.com>
2025-10-10 09:23:49 +0100

    Add files via upload

---
+++ Atividade 13/src/Atividade_13.java
@@ -0,0 +1,13 @@
+public class Atividade_13 {
+    public  static  void main(String[] args){
+
+        int temp = 100;
+
+        if (temp >= 100){
+            System.out.println("A água esta a ferver a temperatura de " + temp);
+        }else {
+            System.out.println("A água nao esta fervendo");
+        }
+    }
+}
+
