5c9e908bf60f8371b0a96bfa48554e964be80de1
Emanuellazaro40 <164494230+Emanuellazaro40@users.noreply.github.com>
2025-10-17 10:25:48 +0100

    Add files via upload

--- Atividade_13.1/src/Atividade_131.java
+++ Atividade_13.1/src/Atividade_131.java
@@ -5,39 +5,49 @@ public class Atividade_131 {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Insere o primeiro numero: ");
-        int num_1 = input.nextInt();
+        double num_1 = input.nextDouble();
 
         System.out.println("Insere o segundo numero: ");
-        int num_2 = input.nextInt();
+        double num_2 = input.nextDouble();
 
         System.out.println("escolha o operador: * + - /:");
         char operador = input.next().charAt(0);
 
         switch (operador){
             case '+':
-                int soma = num_1 + num_2;
+                double soma = num_1 + num_2;
                 System.out.println(soma);
                 break;
             case '-':
-                int sub = num_1 - num_2;
+                double sub = num_1 - num_2;
                 System.out.println(sub);
                 break;
             case '*':
-                int mult = num_1 * num_2;
+                double mult = num_1 * num_2;
                 System.out.println(mult);
                 break;
             case '/':
                 if (num_1 == 0 || num_2 == 0){
-                    System.out.println("Divisao por zero. Impossivel digite outro numero!");
+                    System.out.println("Divisao por zero Impossivel. Digite outro numero!");
+
+                    System.out.println("Insere o primeiro numero: ");
+                    num_1 = input.nextDouble();
+                    System.out.println("Insere o segundo numero: ");
+                    num_2 = input.nextDouble();
+
+                    double div = num_1 / num_2;
+                    System.out.println(div);
+                    break;
                 }else {
-                    int div = num_1 / num_2;
+                    double div = num_1 / num_2;
                     System.out.println(div);
                 }
                 break;
             default:
                 System.out.println("Operador invalido! Tente novamente.");
         }
+
+        input.close(); // fechar o input para nao consumir memoria
     }
 }
-
 
