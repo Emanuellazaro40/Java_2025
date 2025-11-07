d548aa352c4bf83d772e9d2d443e718e5cb32c86
Emanuellazaro40 <164494230+Emanuellazaro40@users.noreply.github.com>
2025-11-07 09:09:03 +0000

    Add files via upload

--- ExerciciosDeExame/Exercicio_1/src/Main.java
+++ ExerciciosDeExame/Exercicio_1/src/Main.java
@@ -1,2 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
 public class Main {
+    public static  void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int nota;
+
+        while (true){
+            System.out.println("Digite uma nota de 0 - 10: ");
+            nota = input.nextInt();
+            if(nota < 0 || nota > 10){
+                System.out.println("Valor invalido!");
+            }
+            else{
+                System.out.println("Valor Valido! " + nota);
+                return;
+            }
+        }
+    }
 }
