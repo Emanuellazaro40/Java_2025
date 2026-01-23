1d09a5efadde07ee9b9b6ca188f2c3e9b4aac38d
Emanuellazaro40 <164494230+Emanuellazaro40@users.noreply.github.com>
2026-01-23 15:59:03 +0000

    Add files via upload

--- Exercicios_23-01/Conversor.java
+++ Exercicios_23-01/Conversor.java
@@ -2,11 +2,28 @@ public class Conversor {
     public static void main(String[] args) {
         
         String idade = "30";
+        int integerIdade = 30;
+        double doubleIdade = 30.5;
+        float floatIdade = 305.9f;
 
+        // Convertendo string para inteiro
         int idadeInteiro = Integer.parseInt(idade);
-        System.out.println("Idade em inteiro: " + idadeInteiro);
+        System.out.println("Idade em string para inteiro: " + idadeInteiro);
 
+        // Convertendo string para float
         float idadeFloat = Float.parseFloat(idade);
-        System.out.println("Idade em float: " + idadeFloat);
+        System.out.println("Idade em string para float: " + idadeFloat);
+
+        // Convertendo double para string
+        String idadeDoubleString = Double.toString(doubleIdade);
+        System.out.println("Idade em double para string: " + idadeDoubleString);
+
+        // Convertendo float para string
+        String idadeFloatString = Float.toString(floatIdade);
+        System.out.println("Idade em float para string: " + idadeFloatString);
+        // Convertendo inteiro para string
+        String idadeString = Integer.toString(integerIdade);
+        System.out.println("Idade em int para string: " + idadeString);
+
     }
 }
